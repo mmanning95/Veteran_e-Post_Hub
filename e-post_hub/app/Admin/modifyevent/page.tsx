294b2b28557d2c98370b5b40dae9a1abb96d7a82
agraves <agraves@eecs.wsu.edu>
2024-10-02 03:00:54 -0700

    Coppied basic page layout from Admin page, further adjustments to come

---
+++ e-post_hub/app/Admin/modifyevent/page.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Link from "next/link";
+
+export default function ModifyEventPage() {
+  return (
+    <div>
+      <h3 className="text-3xl">This will be the modify Event Page</h3>
+
+      {/* Button to go back to Admin page */}
+      <div>
+        <Link href="/admin">Go back to Admin Page</Link>
+      </div>
+
+      {/* Button to go back to Home page */}
+      <div>
+        <Link href="/">Go back to Homepage</Link>
+      </div>
+    </div>
+  );
+}
