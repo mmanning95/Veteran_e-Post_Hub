94b85dfe58b884e763ec2c31cbda1a4dfcef54bf
Melvin M <melvin.manning@wsu.edu>
2024-12-07 15:32:09 -0800

    Updated prisma model to contain comments for events

--- e-post_hub/prisma/migrations/20241120080559_add_settings_model/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Settings" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "key" TEXT NOT NULL,
-    "value" TEXT NOT NULL
-);
-
--- CreateIndex
-CREATE UNIQUE INDEX "Settings_key_key" ON "Settings"("key");
