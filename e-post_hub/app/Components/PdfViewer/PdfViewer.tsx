4c0ebdd0383ee86bb6d3e9e3570b31db63192810
Melvin M <melvin.manning@wsu.edu>
2025-04-21 13:13:43 -0700

    made pdf rendering more lightweight which removes black bar issue

--- e-post_hub/app/Components/PdfViewer/PdfViewer.tsx
+++ e-post_hub/app/Components/PdfViewer/PdfViewer.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import React, { useState } from "react";
-import { Document, Page, pdfjs } from "react-pdf";
+import React, { useEffect, useState } from "react";
+import { pdfjs } from "react-pdf";
 
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
   'pdfjs-dist/build/pdf.worker.min.mjs',
@@ -12,27 +12,43 @@ interface PdfViewerProps {
   containerHeight?: number;
 }
 
-export default function PdfPreview({
-    fileUrl,
-  }: PdfViewerProps) {
-  
-    return (
-      <div>
-        <Document file={fileUrl}>
-          <div
-            style={{
-              width: "100%",
-              height: "400px",
-              overflow: "hidden",
-              position: "relative",
-              }}
-          >
-            <Page
-              pageNumber={1}
-              width={250} 
-            />
-          </div>
-        </Document>
-      </div>
-    );
-  }
+export default function PdfPreview({ fileUrl, containerHeight }: PdfViewerProps) {
+  const [thumbnail, setThumbnail] = useState<string | null>(null);
+
+  const renderPdfThumbnail = async (url: string) => {
+    const loadingTask = pdfjs.getDocument(url);
+    const pdf = await loadingTask.promise;
+    const page = await pdf.getPage(1); // First page
+
+    const viewport = page.getViewport({ scale: 0.5 }); //scale for image size
+    const canvas = document.createElement("canvas");
+    const context = canvas.getContext("2d");
+
+    if (context) {
+      canvas.width = viewport.width;
+      canvas.height = viewport.height;
+      await page.render({ canvasContext: context, viewport }).promise;
+
+      const imageUrl = canvas.toDataURL(); // Get image from canvas
+      setThumbnail(imageUrl);
+    }
+  };
+
+  useEffect(() => {
+    renderPdfThumbnail(fileUrl);
+  }, [fileUrl]);
+
+  return (
+    <div style={{ height: containerHeight || 400, overflow: "hidden" }}>
+      {thumbnail ? (
+        <img
+          src={thumbnail}
+          alt="PDF Thumbnail"
+          style={{ width: "100%", height: "auto", borderRadius: "8px" }}
+        />
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
+}
